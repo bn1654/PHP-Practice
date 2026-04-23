42dd2677f153bbad796dd8f0093a4e653806c6ee
bn1654 <stikenloni@gmail.com>
2026-04-23 10:37:24 +0700

    signup page styles

--- pop-it-mvc/views/site/signup.php
+++ pop-it-mvc/views/site/signup.php
@@ -1,9 +1,17 @@
+<div class="form-wrap">
+<div class="add-form">
 <h2>Регистрация нового пользователя</h2>
 <h3><?= $message ?? ''; ?></h3>
 <form method="post">
+   <div class='inputgr'>
    <label>Логин <input type="text" name="login"></label>
    <label>Роль <input type="text" name="name"></label>
+</div>
+   <div class='inputgr'>
    <label>Пароль <input type="password" name="password"></label>
    <label>Повторите пароль <input type="password" name="password"></label>
+</div>
    <button>Зарегистрировать</button>
 </form>
+</div>
+</div>
