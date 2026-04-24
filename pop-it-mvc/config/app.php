03c857bd2e522e19703b7934817c03b29fc4a340
bn1654 <stikenloni@gmail.com>
2026-04-24 21:53:21 +0700

    base XSS defence middleware

--- pop-it-mvc/config/app.php
+++ pop-it-mvc/config/app.php
@@ -13,6 +13,7 @@ return [
    ],
    'routeAppMiddleware' => [
         'trim' => \Middlewares\TrimMiddleware::class,
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class
     ],
    'validators' => [
        'required' => \Validators\RequireValidator::class,
